5004f87593d9de48e8eb29fb83f93b623e40769c
Geoffrey Aguirre <gkaguirre@me.com>
2020-03-21 09:05:42 -0400

    A file with the standard header comments

---
+++ exampleHeader.m
@@ -0,0 +1,36 @@
+function [ output ] = myFunc( input, varargin )
+% Brief one line description of the function
+%
+% Syntax:
+%   output = myFunc(input)
+%
+% Description:
+%   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+%   nulla a tempor scelerisque. Maecenas et lobortis est. Donec et turpis
+%   sem. Sed fringilla in metus ut malesuada. Pellentesque nec eros
+%   efficitur, pellentesque nisl vel, dapibus felis. Morbi eu gravida enim.
+%   Sed sodales ipsum eget finibus dapibus. Fusce sagittis felis id orci
+%   egestas, non convallis neque porttitor. Proin ut mi augue. Cras posuere
+%   diam at purus dignissim, vel vestibulum tellus ultrices
+%
+% Inputs:
+%   none
+%   foo                   - Scalar. Foo foo foo foo foo foo foo foo foo foo
+%                           foo foo foo foo foo foo foo foo foo foo foo foo
+%                           foo foo foo
+%
+% Optional key/value pairs:
+%   none
+%  'bar'                  - Scalar. Bar bar bar bar bar bar bar bar bar bar
+%                           bar bar bar bar bar bar bar bar bar bar bar bar
+%                           bar bar bar bar bar bar
+%
+% Outputs:
+%   none
+%   baz                   - Cell. Baz baz baz baz baz baz baz baz baz baz
+%                           baz baz baz baz baz baz baz baz baz baz baz baz
+%                           baz baz baz
+%
+% Examples:
+%{
+%}
